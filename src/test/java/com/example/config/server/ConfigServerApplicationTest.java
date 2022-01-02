c03b7202737d9e6c0bb08ea779c10089a5bfd5b3
eralmas7 <eralmas7@yahoo.com>
2022-01-02 22:27:32 +0530

    Fixed warnings + updated configuration.

--- src/test/java/com/example/config/server/ConfigServerApplicationTest.java
+++ src/test/java/com/example/config/server/ConfigServerApplicationTest.java
@@ -2,7 +2,6 @@ package com.example.config.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class ConfigServerApplicationTest {
